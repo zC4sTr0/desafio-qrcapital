045e8298b1f69fcf0202c5b8b482d3f74694fdad
Gabriel Castro <C4sTr0@live.com>
2022-12-12 19:44:02 -0300

    feat: Frontend, Backend and Docker Created project structure with Hello World samples of both. Added Docker support for the project (compose) Changes in gitignore file

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+//hello world nodejs
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(3001, () => {
+  console.log("Example app listening on port 3001!");
+});
