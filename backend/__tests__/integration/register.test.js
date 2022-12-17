10546063c1ca75ab7fc225945403e2c68463c40d
Gabriel Castro <C4sTr0@live.com>
2022-12-17 12:01:30 -0300

    test(backend): TDD - Integration tests Created new tests to check integration with register page and login page

--- backend/__tests__/integration/register.test.js
+++ backend/__tests__/integration/register.test.js
@@ -1,17 +1,33 @@
 const User = require("../../src/models/User");
 
-describe("Registration", () => {
+describe("Registration tests", () => {
+  const random_username = "test" + Math.random().toString(36).substring(2, 15);
   it("should register a new user", async () => {
     const user = User.create({
-      username: "test" + Math.random().toString(36).substring(2, 15),
+      username: random_username,
       password: "test",
       email: "test" + Math.random().toString(36).substring(2, 15) + "@test.com",
       name: "test",
     });
-    console.log(user);
-    var result = await user.save();
-    console.log(result);
-    expect(result).not.toBe(null);
-    expect(user.email).not.toBe(null);
+    var result = await user.register();
+    expect(result.username).toBe(random_username);
+  });
+
+  it("shoud not register a new user with the same username", async () => {
+    const user = User.create({
+      username: random_username,
+      password: "test",
+      email: "test" + Math.random().toString(36).substring(2, 15) + "@test.com",
+      name: "test",
+    });
+    try {
+      var result = await user.register();
+      //if no error is thrown, the test fails
+      expect(result).toBeInstanceOf(Error);
+    } catch (err) {
+      //check if error is a duplicate key error
+      expect(err.code).toBe("23505");
+      expect(err.constraint).toBe("users_username_key");
+    }
   });
 });
--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,4 +1,5 @@
 const db = require("../database/database");
+const bcrypt = require("bcrypt");
 
 class User {
   constructor(user) {
@@ -8,11 +9,21 @@ class User {
     this.name = user.name;
   }
 
+  async cryptUserPassword() {
+    try {
+      const salt = await bcrypt.genSalt(10);
+      this.password = await bcrypt.hash(this.password, salt);
+    } catch (err) {
+      console.log("Error while hashing the password" + err);
+    }
+  }
+
   static create(user) {
     return new User(user);
   }
 
-  async save() {
+  async register() {
+    await this.cryptUserPassword();
     const query = `
         INSERT INTO users (ID, username, password, email, name) 
         VALUES (nextval('users_id_seq'), $1, $2, $3, $4) RETURNING *;
